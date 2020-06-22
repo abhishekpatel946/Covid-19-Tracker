994f210573c7f7a187eff5aac7435d93aa73f205
abhishek patel <abhishekpatel946@gmail.com>
2020-06-22 15:39:42 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# Covid-19 STATS
+
+Copyright 2020 Covid-19 STATS
+Written by: * **Abhishek Patel** - (https://github.com/abhishekpatel946)
+
+Covid-19 STATS based on geolocation IP. Total Cases, Recovered and Deaths are draw using linear chart.
+
+### Cloning:
+```
+git clone https://github.com/abhishekpatel946/
+```
+
+### Running:
+```
+cd Covid-19 Tracker
+```
+
+```
+sudo su
+```
+
+```
+index.html
+```
+
+## Features 
+
+- live recoreds based on your geolocation.
+- Total Cases with new cases (if occurs).
+- Total Recovered Cases with with new recovers (if recovers).
+- Total Deaths with new deaths (if occurs).
+- Check other 203 countries cases.
+- Linear Chart for easy to readable.
+
+### Prerequisites
+
+* Browser
+
+### Install requirements
+
+* pip install cryptography
+* pip install gspread
+* pip install oauth2client
+
+### Tested on:
+
++ Kali Linux - ROLLING
+
+### Screenshot:
+![Shot](https://github.com/abhishekpatel946/)
+
+More in [Screens](Screens)
+
+### Contribute:
+Send me more features if you want it :D
+
+**I need your Help to become it to better.**
+
+### Contact:
+**abhishekpatel946@gmail.com**
---
+++ js/app.js
@@ -0,0 +1,143 @@
+
+// SELECT ALL ELEMENTS
+const country_name_element = document.querySelector(".country .name");
+const total_cases_element = document.querySelector(".total-cases .value");
+const new_cases_element = document.querySelector(".total-cases .new-value");
+const recovered_element = document.querySelector(".recovered .value");
+const new_recovered_element = document.querySelector(".recovered .new-value");
+const deaths_element = document.querySelector(".deaths .value");
+const new_deaths_element = document.querySelector(".deaths .new-value");
+
+const ctx = document.getElementById("axes_line_chart").getContext("2d");
+
+// APP VARIABLES
+let app_data = [],
+	cases_list = [],
+	recovered_list = [],
+	deaths_list = [],
+	deaths = [],
+	formatedDates = [];
+
+// GET USERS COUNTRY CODE
+let country_code = geoplugin_countryCode();
+let user_country;
+country_list.forEach( country => {
+	if( country.code == country_code ){
+		user_country = country.name;
+	}
+});
+
+/* ---------------------------------------------- */
+/*                API URL AND KEY                 */
+/* ---------------------------------------------- */
+
+function fetchData(user_country){
+	country_name_element.innerHTML = "Loading...";
+
+	cases_list = [], recovered_list =[], deaths_list = [], dates = [], formatedDates = [];
+	
+	fetch(`https://covid19-monitor-pro.p.rapidapi.com/coronavirus/cases_by_days_by_country.php?country=${user_country}`, {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "covid19-monitor-pro.p.rapidapi.com",
+			"x-rapidapi-key": "7e269ec140msh8a5df9cfc21b4b4p1c1e3ejsn9aba26afc6e0"
+		}
+	})
+	.then( response => {
+		return response.json();
+	})
+	.then( data => {
+		dates = Object.keys(data);
+		
+		dates.forEach( date => {
+			let DATA = data[date];
+
+			formatedDates.push(formatDate(date));
+			app_data.push(DATA);
+			cases_list.push(parseInt(DATA.total_cases.replace(/,/g, "")));
+			recovered_list.push(parseInt(DATA.total_recovered.replace(/,/g, "")));
+			deaths_list.push(parseInt(DATA.total_deaths.replace(/,/g, "")));
+		})
+	})
+	.then( () => {
+		updateUI();
+	})
+	.catch( error => {
+		alert(error);
+	})
+}
+
+fetchData(user_country);
+
+// UPDATE UI FUNCTION
+function updateUI(){
+	updateStats();
+	axesLinearChart();
+}
+
+function updateStats(){
+	let last_entry = app_data[app_data.length - 1];
+	let before_last_entry = app_data[app_data.length - 2];
+
+	country_name_element.innerHTML = last_entry.country_name;
+
+	total_cases_element.innerHTML = last_entry.total_cases || 0;
+	new_cases_element.innerHTML = `+${last_entry.new_cases || 0 }`;
+
+	recovered_element.innerHTML = last_entry.total_recovered || 0;
+	new_recovered_element.innerHTML = `+${parseInt(last_entry.total_recovered.replace(/,/g, "")) - parseInt(before_last_entry.total_recovered.replace(/,/g, ""))}`;
+	
+	deaths_element.innerHTML = last_entry.total_deaths;
+	new_deaths_element.innerHTML = `+${last_entry.new_deaths || 0}`;
+}
+
+// UPDATE CHART
+let my_chart;
+function axesLinearChart(){
+
+	if(my_chart){
+		my_chart.destroy();
+	}
+
+	my_chart = new Chart(ctx, {
+		type: 'line',
+		data: {
+			datasets: [{
+				label: 'Cases',
+				data: cases_list,
+				fill : false,
+				borderColor : '#d0f6d3de',
+				backgroundColor: '#d0f6d3de',
+				borderWidth : 0
+			},{
+				label: 'Recovered',
+				data: recovered_list,
+				fill : false,
+				borderColor : '#00ff15dc',
+				backgroundColor: '#00ff15dc',
+				borderWidth : 0
+			},{
+				label: 'Deaths',
+				data: deaths_list,
+				fill : false,
+				borderColor : '#e21405d5',
+				backgroundColor: '#e21405d5',
+				borderWidth : 0
+			}],
+			labels: formatedDates
+		},
+		options: {
+			responsive : true,
+			maintainAspectRatio : false
+		}
+	});
+}
+
+// FORMAT DATES
+const monthsNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+function formatDate(dateString){
+	let date = new Date(dateString);
+
+	return `${date.getDate()} ${monthsNames[date.getMonth()]}`;
+}
